584f607d7c3539cc4bd61dcea83e390d60fe35f2
Bhargav Jodhani <bhargav.jodhani@samptel.in>
2025-04-22 18:53:48 +0530

    added firebase configuration -3

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ if ("serviceWorker" in navigator) {
     .register("sw.js")
     .then((registration) => {
       console.log("Service Worker registered with scope:", registration.scope);
+      console.log("code updated");
+      
     })
     .catch((error) => {
       console.error("Service Worker registration failed:", error);
